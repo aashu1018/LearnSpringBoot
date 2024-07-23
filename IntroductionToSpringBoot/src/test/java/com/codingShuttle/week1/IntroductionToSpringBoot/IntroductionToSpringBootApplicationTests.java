40a16ec12139f08265773f47507d1a3c78fd8352
Ashutosh <ashu.ksing1018@gmail.com>
2024-07-23 22:46:50 +0530

    push to git

---
+++ IntroductionToSpringBoot/src/test/java/com/codingShuttle/week1/IntroductionToSpringBoot/IntroductionToSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codingShuttle.week1.IntroductionToSpringBoot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IntroductionToSpringBootApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
